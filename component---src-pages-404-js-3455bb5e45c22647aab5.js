d1e5eebca7c3c6e4ac7e56b64b16f8810a83f65e
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2023-10-01 22:56:35 +0500

    Update

---
+++ component---src-pages-404-js-3455bb5e45c22647aab5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t),r=a("Ws0g"),o=a("7oih"),c=a("jWPW");n.default=()=>l.a.createElement(o.a,null,l.a.createElement(r.a,null,l.a.createElement(r.b,null,"404"),l.a.createElement(c.a,null,"Oops, nothing here. Try other page.")))}}]);
+//# sourceMappingURL=component---src-pages-404-js-3455bb5e45c22647aab5.js.map
